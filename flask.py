56c2028b776f2982b80211e4fa2bc954b71b18f0
Bryant-Huang-BU <115976062+Bryant-Huang-BU@users.noreply.github.com>
2023-11-19 00:09:45 -0600

    added flask tests

--- flask.py
+++ flask.py
@@ -12,3 +12,12 @@ def post():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        user = request.form['nm']
+        return redirect(url_for('success', name=user))
+    else:
+        user = request.args.get('nm')
+        return redirect(url_for('success', name=user))
