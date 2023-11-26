cda7fd88bec0a8e9da084f6bd74fbad6ff405b50
Bryant-Huang-BU <Bryant_Huang1@Baylor.edu>
2023-11-26 00:18:54 -0600

    added a <id>

--- manager.py
+++ manager.py
@@ -1,7 +1,7 @@
 def manager(id):
     return '''<html>
    <body>
-      <form action = "http://localhost:5000/manager" method = "get">
+      <form action = "http://localhost:5000/manager/<id>" method = "get">
           <h2>Results for ''' + id + ''':</h2>
       </form>
    </body>
